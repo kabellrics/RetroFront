aeaf69765412981acc2c77c2ac777cdb469f3908
Flechel Yannis <yflechel@gmail.com>
2021-01-31 15:11:40 +0100

    Carrousel View Animations

--- RetroFront/PlateformeView/CarrouselSystemView.xaml.cs
+++ RetroFront/PlateformeView/CarrouselSystemView.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,5 +24,31 @@ namespace RetroFront.PlateformeView
             InitializeComponent();
         }
 
+        private void listicon_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                int itemwidht = 316;
+                var previousmargin = listicon.Margin;
+                var nextmargin = previousmargin;
+                nextmargin.Left = -itemwidht * listicon.SelectedIndex;
+                var sb = new Storyboard();
+                var ta = new ThicknessAnimation();
+                ta.BeginTime = new TimeSpan(0);
+                ta.SetValue(Storyboard.TargetNameProperty, "listicon");
+                Storyboard.SetTargetProperty(ta, new PropertyPath(MarginProperty));
+
+                ta.From = previousmargin;
+                ta.To = nextmargin;
+                ta.Duration = new Duration(TimeSpan.FromMilliseconds(500));
+
+                sb.Children.Add(ta);
+                sb.Begin(this);
+            }
+            catch (Exception ex)
+            {
+                //throw;
+            }
+        }
     }
 }
